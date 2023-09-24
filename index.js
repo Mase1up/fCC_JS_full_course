f67693d7280da22c27d2ddfbff11f37ac12b937c
Mase1up <mason.l.keith@gmail.com>
2023-09-24 01:02:24 -0400

    Create function adding variables

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 // document.getElementById("count").innerText = 5;
 
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
 
-// Setting up the race
+// Create function logs out sum of all laps
 
-function countdown() {
-    console.log(5)
-    console.log(4)
-    console.log(3)
-    console.log(2)
-    console.log(1)
+function totalTime() {
+    let lapTotals = lap1 + lap2 + lap3;
+    console.log(lapTotals);
 }
 
-countdown();
-
-// Get another race ready...
-
-countdown();
+totalTime();
