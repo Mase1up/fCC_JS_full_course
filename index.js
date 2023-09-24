4edb8e20f38ecef01784a81cc49f4699ff4c19e2
Mase1up <mason.l.keith@gmail.com>
2023-09-24 01:25:08 -0400

    Input pseudo-code

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-// document.getElementById("count").innerText = 5;
+// document.getElementById("count-el").innerText = 5;
 
-let lapsCompleted = 0;
-
-// Create function that increments lapsCompleted
-
-function addLap() {
-    lapsCompleted++;
-}
-
-addLap();
-addLap();
-addLap();
-
-console.log(lapsCompleted);
-
-// I keep forgetting to actually log to console
-// Spent way too long trying to figure out what I 
-// did wrong.
+// Initialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when button is clicked
+// change the count-el in the HTML to reflect the new count
