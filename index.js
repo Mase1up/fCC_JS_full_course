7975cdbc942ac69ba91429d2711048b936ab7f9c
Mase1up <mason.l.keith@gmail.com>
2023-09-24 01:36:20 -0400

    Add event listener to HTML, func in JS

--- index.js
+++ index.js
@@ -4,3 +4,9 @@
 // listen for clicks on the increment button
 // increment the count variable when button is clicked
 // change the count-el in the HTML to reflect the new count
+
+let count = 0;
+
+function increment() {
+    console.log("clicked")
+}
