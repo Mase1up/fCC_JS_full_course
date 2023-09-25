9dec0c74555ed534e53cc8dda1ee18aa634be06d
Mase1up <mason.l.keith@gmail.com>
2023-09-24 22:28:28 -0400

    Slightly modify previous if... logic

--- 2. Blackjack/index.js
+++ 2. Blackjack/index.js
@@ -37,10 +37,11 @@ let sum = firstCard + secondCard;
 // let secondCard = 9;
 // let sum = firstCard + secondCard;
 
-if (sum < 21) {
+if (sum <= 20) {
     console.log("Do you want to Hit?");
 } else if (sum === 21) {
     console.log("Blackjack!  Congratulations!");
 } else {
-    console.log("Sorry, you busted");
+    console.log("You busted, sorry!");
 }
+
