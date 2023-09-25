0d183cea4735d1bd46e5e42a4d679d16b5720ca7
Mase1up <mason.l.keith@gmail.com>
2023-09-25 13:58:58 -0400

    Deal new hand with ever START click

--- 2. Blackjack/index.js
+++ 2. Blackjack/index.js
@@ -6,7 +6,7 @@
 
 // 2. Create a var, sum, and set it to the sum of the two cards.
 
-let randomCard = Math.floor(Math.random() * ((11-2)+1)+2); 
+// let randomCard = Math.floor(Math.random() * ((11-2)+1)+2); 
 // I had to look up how to generate a random integer between two inclusive values
 // That gave me: Math.floor(Math.random() * ((max-min)+1) + min )
 // The above code should give a random integer 2-11 inclusive to randomCard
@@ -17,17 +17,21 @@ function dealCard() {
     return card = Math.floor(Math.random() * ((11-2)+1)+2); 
 }
 
+function dealHand() {
 dealCard();
 let firstCard = card;
     // console.log(firstCard)
 dealCard();
 let secondCard = card;
     // console.log(secondCard)
-let sum = firstCard + secondCard;
+return sum = firstCard + secondCard;
     // console.log(sum)
+}
+
 let hasBlackjack = false;
 let isAlive = true;
 let message = "";
+
 // There we go!  I feel like I was supposed to find another way, because
 // at this stage we were not introduced to return...
 // but it works, and that makes me happy!
@@ -41,20 +45,38 @@ let message = "";
 // 2. Create a startGame() func. Move the conditional below
 // line (44 - 55) inside the body of the function
 
-if (sum <= 20) {
-    message = "Do you want to Hit?";
-} else if (sum === 21) {
-    message = "Blackjack!  Congratulations!";
-    hasBlackjack = true;
-} else {
-    message = "You busted, sorry!";
-    isAlive = false;
-}
-
-// Log it out!
-console.log(message);
-
-
 function startGame() {
-    console.log("Game Started!")
+
+    dealHand();
+
+    if (sum <= 20) {
+        message = "Do you want to Hit?";
+    } else if (sum === 21) {
+        message = "Blackjack!  Congratulations!";
+        hasBlackjack = true;
+    } else {
+        message = "You busted, sorry!";
+        isAlive = false;
+    }
+    console.log(sum);
+    console.log(message);
 }
+
+// The issue I'm having is the random cards are set at page load
+// I think I need to move my random card generation INSIDE the 
+// function so that every button push generates new cards.
+// 
+// What I tried, is moving the entire dealCard process INTO its 
+// own function dealHand()...wait, can I simply invoke the function
+// INSIDE startGame()?  Try that first, I like that way better.
+
+// That's giving me an error that sum is not defined, so I'm 
+// taking that to mean I CANNOT invoke the function dealHand()
+// inside startGame(), however I really think it just means I'm 
+// not doing it the right way, and I'll learn later how to call
+// a function inside another one.
+
+// First thing I realized I need to return the value of sum, so
+// changed let to return.
+
+// SUCCESS!!!!!
