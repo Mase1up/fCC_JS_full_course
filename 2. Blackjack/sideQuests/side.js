09846512173549b7b452e8de100dac66ca6169f1
Mase1up <mason.l.keith@gmail.com>
2023-09-24 22:14:58 -0400

    Set up 100 bday problem

--- 2. Blackjack/sideQuests/side.js
+++ 2. Blackjack/sideQuests/side.js
@@ -3,14 +3,22 @@
 // Check if the person is old enough to enter the nightclub (21)
 // Log suitable message to the console in both case
 
-let age = 22;
+// let age = 22;
 
 // if less than 21 -> "You can not enter the club!"
 // if greater than 21 -> "Welcome!"
 
-if (age < 21) {
-    console.log("You can't enter the club!");
-} else {
-    console.log("Welcome!");
-}
+// if (age < 21) {
+    // console.log("You can't enter the club!");
+// } else {
+    // console.log("Welcome!");
+// }
+
+// Check if person is eligible for birthday card from the King! (100)
+
+let age = 100;
+
+// if less than 100 -> "Not eligible"
+// if exactly 100 -> "Here is your birthday card from the King!"
+// if greater than 100, "Not elegible, you've already gotten one!"
 
