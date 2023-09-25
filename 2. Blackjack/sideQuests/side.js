3f72941c24156d27ac5b67b7f76e93d6935919e8
Mase1up <mason.l.keith@gmail.com>
2023-09-24 22:01:37 -0400

    Add sideQuest folder to Blackjack project

---
+++ 2. Blackjack/sideQuests/side.js
@@ -0,0 +1,10 @@
+// Side lessons during the Blackjack project
+
+// Check if the person is old enough to enter the nightclub (21)
+// Log suitable message to the console in both case
+
+let age = 22;
+
+// if less than 21 -> "You can not enter the club!"
+// if greater than 21 -> "Welcome!"
+
