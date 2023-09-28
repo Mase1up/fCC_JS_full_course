f13163f580b27d10c66d2c99324b46c255dc9c64
Mase1up <mason.l.keith@gmail.com>
2023-09-28 11:38:25 -0400

    Update the other getElementbyId to querySelector

--- 2. Blackjack/index.js
+++ 2. Blackjack/index.js
@@ -45,7 +45,7 @@ function handTotal() {
 let hasBlackjack = false;
 let isAlive = true;
 let message = "";
-let messageEl = document.getElementById("message-el");
+let messageEl = document.querySelector("#message-el");
 // let sumEl = document.getElementById("sum-el");
 let sumEl = document.querySelector("#sum-el");
 
