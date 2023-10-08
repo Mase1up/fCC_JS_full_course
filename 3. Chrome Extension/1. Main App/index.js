9672a54a4f1ffb5854500da10427017f928f2035
Mase1up <mason.l.keith@gmail.com>
2023-10-07 23:16:51 -0400

    Add variables, and set as const instead of let

--- 3. Chrome Extension/1. Main App/index.js
+++ 3. Chrome Extension/1. Main App/index.js
@@ -1,8 +1,13 @@
-// Log out "Button clicked!" when the user clicks SAVE INPUT button
+// Create two variables:
+// myLeads -> should be assigned to empty array
+// inputEl -> should be assigned to the text input field
 
-let inputBtn = document.querySelector('#input-btn');
+
+let myLeads = [];
+const inputEl = document.querySelector("#input-el");
+const inputBtn = document.querySelector('#input-btn');
 
 inputBtn.addEventListener("click", function() {
-    console.log("Button clicked from addEventListener!");
+    console.log("Button clicked!");
 })
 
