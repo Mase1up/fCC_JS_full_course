55d6a373960c59c8b07c3d4f06dd0629e3996f37
Mase1up <mason.l.keith@gmail.com>
2023-10-07 23:36:20 -0400

    Add ul to HTML, create const to store it in JS for adding data

--- 3. Chrome Extension/1. Main App/index.js
+++ 3. Chrome Extension/1. Main App/index.js
@@ -1,14 +1,14 @@
 let myLeads = ["www.ultimate-sven.com", "www.mason-keith.com", "www.vanessa-keith.com"];
 const inputEl = document.querySelector("#input-el");
 const inputBtn = document.querySelector('#input-btn');
+// 2. Grab the unordered list and store it in a const called ulEl
+const ulEl = document.querySelector('#ul-el');
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
     console.log(myLeads);
 })
 
-
-// Log out the items in the myLead array using a for loop (hard-coded leads for now)
 
 for (let i = 0; i < myLeads.length; i++) {
     console.log(myLeads[i]);
