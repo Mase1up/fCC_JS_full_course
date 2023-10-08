663524c4308091596340b7202198c9278321f5c8
Mase1up <mason.l.keith@gmail.com>
2023-10-07 22:57:28 -0400

    Change onclick to addEventListener

--- 3. Chrome Extension/index.js
+++ 3. Chrome Extension/index.js
@@ -1,5 +1,12 @@
 // Log out "Button clicked!" when the user clicks SAVE INPUT button
 
 function saveInput() {
-    console.log("Button clicked!");
+    console.log("Button clicked from onclick attribute");
 }
+
+let inputBtn = document.querySelector('#input-btn');
+
+inputBtn.addEventListener("click", function() {
+    console.log("Button clicked from addEventListener!");
+})
+
