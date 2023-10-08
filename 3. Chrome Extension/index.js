18193ff9b40fb0c80d09ad2002e53993a55d84ab
Mase1up <mason.l.keith@gmail.com>
2023-10-07 22:58:09 -0400

    Remove old function that was for onclick

--- 3. Chrome Extension/index.js
+++ 3. Chrome Extension/index.js
@@ -1,9 +1,5 @@
 // Log out "Button clicked!" when the user clicks SAVE INPUT button
 
-function saveInput() {
-    console.log("Button clicked from onclick attribute");
-}
-
 let inputBtn = document.querySelector('#input-btn');
 
 inputBtn.addEventListener("click", function() {
