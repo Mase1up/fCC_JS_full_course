0a1cb85fb7c1abb06add153532917bdc3048b097
Mase1up <mason.l.keith@gmail.com>
2023-10-08 20:22:48 -0400

    Finish innerHTML challenge

--- 3. Chrome Extension/Challenges/index.js
+++ 3. Chrome Extension/Challenges/index.js
@@ -2,5 +2,11 @@
 
 const divContainer = document.querySelector("#container");
 
-divContainer.innerHTML = "<button>Buy!</button>";
+divContainer.innerHTML = "<button onclick='buy()'>Buy!</button>";
 
+// When clicked, render a paragraph under the button (in the container)
+// that says "Thank you for buying!""
+
+function buy() {
+    divContainer.innerHTML += "<p>Thank you for buying!</p>";
+}
