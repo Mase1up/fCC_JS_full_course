cb9969c52f4a39373ce231afb6b36268ec9ea138
Mase1up <mason.l.keith@gmail.com>
2023-10-08 20:46:51 -0400

    Turn second piece into another function, call on btn-click

--- 3. Chrome Extension/1. Main App/index.js
+++ 3. Chrome Extension/1. Main App/index.js
@@ -1,4 +1,4 @@
-let myLeads = ["www.ultimate-sven.com", "www.mason-keith.com", "www.vanessa-keith.com"];
+let myLeads = [];
 const inputEl = document.querySelector("#input-el");
 const inputBtn = document.querySelector('#input-btn');
 const ulEl = document.querySelector('#ul-el');
@@ -6,16 +6,15 @@ const ulEl = document.querySelector('#ul-el');
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
     console.log(myLeads);
+    //2. Call the renderLeads() function
+    renderLeads();
 })
 
-// 1. Create a variable, listItems, to hold all the HTML for the list items
-// Assign it an empty string to begin with
-let listItems = "";
-
-for (let i = 0; i < myLeads.length; i++) {
-    // 2. Add the item to the listItems variable instead of the ulEl.innerHTML
-    listItems += "<li>" + myLeads[i] + "</li>";
-
+// 1. Wrap the code below in a func renderLeads()
+function renderLeads() {
+    let listItems = "";
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += "<li>" + myLeads[i] + "</li>";
+    }
+    ulEl.innerHTML = listItems;
 }
-// 3. Render the listItems inside the unordered list using ulEL.innerHTML
-ulEl.innerHTML = listItems;
