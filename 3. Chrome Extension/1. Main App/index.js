b661e0e9ca0d7801f389bd4c691101ed33fb1fdd
Mase1up <mason.l.keith@gmail.com>
2023-10-14 22:40:50 -0400

    Ready to turn <li> into a link

--- 3. Chrome Extension/1. Main App/index.js
+++ 3. Chrome Extension/1. Main App/index.js
@@ -5,37 +5,16 @@ const ulEl = document.querySelector('#ul-el');
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
-    // Clear out the input field
     inputEl.value = "";
     renderLeads();
 })
 
-// 1. Wrap the code below in a func renderLeads()
 function renderLeads() {
     let listItems = "";
     for (let i = 0; i < myLeads.length; i++) {
+        // Make the <li> a link to the site and open in a new tab
+        // <li><a href="https://www.mason-keith.com" target="_blank">www.mason-keith.com</a></li>
         listItems += "<li>" + myLeads[i] + "</li>";
     }
     ulEl.innerHTML = listItems;
 }
-    // So his version, you can omit setting the listItems variable altogether;
-    // function renderLeads() {
-    //     for( let i = 0; i < myLeads.length; i++) {
-    //         ulEl.innerHTML += "<li>" + myLeads[i] = "</li>";
-    //     }
-    // }
-
-
-    // Alternatively, you can use a second method completely
-    //
-    //      for (let i = 0; i < myLeads.length; i++) {
-    //          const li = document.createElement("li");
-    //          li.textContent = myLeads[i];
-    //          ulEl.append(li);
-    //}
-
-
-// You have to be kidding me.... After ALLLLL that, he wants us to do the code EXACTLY
-// as I originally did it, creating a listItems variable, adding each <li> to it, then after the
-// loop, set ulEl = listItems;
-
