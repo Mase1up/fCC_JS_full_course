63fec4bc08b5b5d38edb8e953588dcac2eed0931
Mase1up <mason.l.keith@gmail.com>
2023-10-24 23:16:30 -0400

    Complete template string for sender & recipient

--- 3. Chrome Extension/Challenges/index.js
+++ 3. Chrome Extension/Challenges/index.js
@@ -15,8 +15,9 @@ function buy() {
 // Template strings -or- Template literals
 
 const recipient = "James";
+const sender = "Mason";
 
 // Refactor the email string to use template strings
-const email = `Hey ${recipient}!  How is it going?  Cheers Per`;
+const email = `Hey ${recipient}!  How is it going?  Cheers, ${sender}`;
 
 console.log(email);
